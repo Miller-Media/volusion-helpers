f87a87086a29ce27b797812f806bb3ed514c1fe8
Matt Miller <matt@miller-media.com>
2016-02-05 11:22:26 -0800

    Initial Commit

---
+++ helper-functions.js
@@ -0,0 +1,24 @@
+var VOLUSION_HELPERS = VOLUSION_HELPERS || {};
+
+VOLUSION_HELPERS.detect = {
+	isHome: function(){
+		if(location.pathname.toLowerCase('default') > -1 || location.pathname=="/"){
+			return true;
+		}
+		else{
+			return false;
+		}
+	},
+	isCategory: function(){
+
+	},
+	isProductPage: function(){
+
+	}
+};
+
+VOLUSION_HELPERS.layout = {
+	moveGoButton: function(){
+		
+	}
+};
