ead56773a1854710cd5feabf830ea7f403d1855c
Matt Miller <matt@miller-media.com>
2016-02-17 11:20:45 -0800

    Support for Products / Categories / Shopping Cart / One Page Checkout

--- helper-functions.js
+++ helper-functions.js
@@ -2,18 +2,24 @@ var VOLUSION_HELPERS = VOLUSION_HELPERS || {};
 
 VOLUSION_HELPERS.detect = {
 	isHome: function(){
-		if(location.pathname.toLowerCase('default') > -1 || location.pathname=="/"){
-			return true;
-		}
-		else{
-			return false;
-		}
+		returnValue = location.pathname.toLowerCase('default') > -1 || location.pathname=="/") ? true : false;
+		return returnValue;
 	},
 	isCategory: function(){
-
+		returnValue = location.pathname.toLowerCase('_s') > -1 || location.pathname.toLowerCase('-s') > -1 || location.pathname.toLowerCase('searchresults') > -1 ? true : false;
+		return returnValue;
 	},
 	isProductPage: function(){
-
+		returnValue = location.pathname.toLowerCase('_p') > -1 || location.pathname.toLowerCase('-p') > -1 || location.pathname.toLowerCase('productdetails') > -1 ? true : false;
+		return returnValue;
+	},
+	isCheckout: function(){
+		returnValue = location.pathname.toLowerCase('one-page-checkout') > -1 ? true : false;
+		return returnValue;
+	},
+	isShoppingCart: function(){
+		returnValue = location.pathname.toLowerCase('shoppingcart') > -1 ? true : false;
+		return returnValue;
 	}
 };
 
