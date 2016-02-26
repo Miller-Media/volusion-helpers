aedf7173b13f457aa684cc651ef62758b6ed2a41
Matt Miller <matt@miller-media.com>
2016-02-25 17:53:14 -0800

    Cookie Functions / GET variables

--- helper-functions.js
+++ helper-functions.js
@@ -28,3 +28,34 @@ VOLUSION_HELPERS.layout = {
 		
 	}
 };
+
+VOLUSION_HELPERS.helper = {
+	getParameterByName: function(name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+};
+
+VOLUSION_HELPERS.cookies = {
+	setCookie: function(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	},
+	getCookie: function(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+		return "";
+	}
+};
