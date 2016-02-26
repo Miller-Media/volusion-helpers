35a49d98af6648266f25dd81b6a01a539e0583c5
Matt Miller <matt@miller-media.com>
2016-02-26 10:19:29 -0800

    Bug fixes

--- helper-functions.js
+++ helper-functions.js
@@ -6,19 +6,19 @@ VOLUSION_HELPERS.detect = {
 		return returnValue;
 	},
 	isCategory: function(){
-		returnValue = location.pathname.toLowerCase.indexOf('_s') > -1 || location.pathname.toLowerCase().indexOf('-s') > -1 || location.pathname.toLowerCase().indexOf('searchresults') > -1 ? true : false;
+		returnValue = location.pathname.toLowerCase().indexOf('_s') > -1 || location.pathname.toLowerCase().indexOf('-s') > -1 || location.pathname.toLowerCase().indexOf('searchresults') > -1 ? true : false;
 		return returnValue;
 	},
 	isProductPage: function(){
-		returnValue = location.pathname.toLowerCase.indexOf('_p') > -1 || location.pathname.toLowerCase().indexOf('-p') > -1 || location.pathname.toLowerCase().indexOf('productdetails') > -1 ? true : false;
+		returnValue = location.pathname.toLowerCase().indexOf('_p') > -1 || location.pathname.toLowerCase().indexOf('-p') > -1 || location.pathname.toLowerCase().indexOf('productdetails') > -1 ? true : false;
 		return returnValue;
 	},
 	isCheckout: function(){
-		returnValue = location.pathname.toLowerCase.indexOf('one-page-checkout') > -1 ? true : false;
+		returnValue = location.pathname.toLowerCase().indexOf('one-page-checkout') > -1 ? true : false;
 		return returnValue;
 	},
 	isShoppingCart: function(){
-		returnValue = location.pathname.toLowerCase.indexOf('shoppingcart') > -1 ? true : false;
+		returnValue = location.pathname.toLowerCase().indexOf('shoppingcart') > -1 ? true : false;
 		return returnValue;
 	}
 };
