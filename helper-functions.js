4bef253647e387360fccc84c34983342bfae9792
Matt Miller <matt@miller-media.com>
2017-03-09 10:30:48 -0800

    Cookie update

--- helper-functions.js
+++ helper-functions.js
@@ -56,7 +56,7 @@ VOLUSION_HELPERS.cookies = {
 		var d = new Date();
 		d.setTime(d.getTime() + (exdays*24*60*60*1000));
 		var expires = "expires="+d.toUTCString();
-		document.cookie = cname + "=" + cvalue + "; " + expires;
+		document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
 	},
 	getCookie: function(cname) {
 		var name = cname + "=";
